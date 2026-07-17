64d38750e195cbbc758d1c98f588c7f24682b805
mberd7 <134493326+mberd7@users.noreply.github.com>
2026-07-18 01:30:51 +0400

    მთავარი ლოგიკა, შესალამაზებელი

--- hangman_game/main.py
+++ hangman_game/main.py
@@ -14,41 +14,51 @@ def new_format(ch, real_word, hidden_format ) -> str:
 
    return final_format
          
+def not_valid(guessed: str):
+   return   len(guessed) != 1 or not guessed.isalpha() 
         
 
 
 
-lives = 6 # სისცოცხლის რაოდენობა
+
+def main():
+ lives = 6 # სისცოცხლის რაოდენობა
 
 
-hidden_word = random.choice(wordlist.word_list) #ირჩევს გამოსაცნობ სიტყვას
+ hidden_word = random.choice(wordlist.word_list) #ირჩევს გამოსაცნობ სიტყვას
 
-guessed_chars = list()
+ guessed_chars = list()
 
-print(hidden_word)
-hidden = '*' * len(hidden_word)
+ print(hidden_word)
+ hidden = '*' * len(hidden_word)
+ print(hidden)
 
-while(True):
-    print(hidden)
-    print(f"გამოცნობილი ასოები: {guessed_chars}")
+ while(True):
+    
     guessed_char = input("შეიყვანეთ სავარაუდო ასე: ")
 
-    if guessed_char in hidden_word:
+    if(not_valid(guessed_char)):
+       print("არასწორი ფორმატით შეიყვანეთ, გთხოვთ ცადეთ ხელთავიდან:")
+    else:  
+       print(hidden)
+
+       if guessed_char in hidden_word:
         print("ყოჩაღ, სწორად გამოიცანი!")
         hidden = new_format(guessed_char, hidden_word, hidden)
         guessed_chars.append(guessed_char)
 
-    elif(guessed_char not in hidden_word and lives != 1):
-      lives -= 1
-      print(f"ცადეთ ხელთავიდან,დარჩენილი გაქვთ {lives} ცდა")
+       elif(guessed_char not in hidden_word and lives != 1):
+         lives -= 1
+         print(f"ცადეთ ხელთავიდან,დარჩენილი გაქვთ {lives} ცდა")
 
-    else:
-      print(f"სამწუხაროდ, ცდა ამოგეწურათ")
-      break
+       else:
+        print(f"სამწუხაროდ, ცდა ამოგეწურათ")
+        break
 
-    if hidden_word == hidden:
-       print("ყოჩაღ! შენ სწორად გამოიცანი სიტყვა!")
-       break
+       if hidden_word == hidden:
+         print("ყოჩაღ! შენ სწორად გამოიცანი სიტყვა!")
+         break
+    print(f"გამოცნობილი ასოები: {guessed_chars}")
        
     
-
+main()
