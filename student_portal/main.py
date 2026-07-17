9235bdf2102752560000c8ec29fbbc6f6e02c4db
FeynmansFailedFan <mberd1932@gmail.com>
2026-07-18 01:56:40 +0400

    სტუდენტის კლასის ფუნქციების დამატება(კოდის გარეშე)

--- student_portal/main.py
+++ student_portal/main.py
@@ -5,4 +5,18 @@ class Student:
         self.roll_numer = roll_number
         self.grade = grade
 
+    def add_new_student(self, name, roll_number, grade):
+        ...
+
+    def show_all_students():
+        ...
+
+    def find_student(self, roll_number):
+        ...
+
+    def change_grade(self, roll_number):
+        ...
+
+    def leave_system():
+        ...
     
