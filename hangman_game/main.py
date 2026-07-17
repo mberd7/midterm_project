74fd6f3007b617fa0e630f71d1d4f2d68be26397
FeynmansFailedFan <mberd1932@gmail.com>
2026-07-17 22:27:53 +0400

    დავამატე სიტყვის სრულიად გამოცნობის შემთხვევა

--- hangman_game/main.py
+++ hangman_game/main.py
@@ -23,23 +23,32 @@ lives = 6 # სისცოცხლის რაოდენობა
 
 hidden_word = random.choice(wordlist.word_list) #ირჩევს გამოსაცნობ სიტყვას
 
+guessed_chars = list()
+
 print(hidden_word)
 hidden = '*' * len(hidden_word)
 
 while(True):
     print(hidden)
+    print(f"გამოცნობილი ასოები: {guessed_chars}")
     guessed_char = input("შეიყვანეთ სავარაუდო ასე: ")
 
     if guessed_char in hidden_word:
         print("ყოჩაღ, სწორად გამოიცანი!")
         hidden = new_format(guessed_char, hidden_word, hidden)
-        
+        guessed_chars.append(guessed_char)
+
     elif(guessed_char not in hidden_word and lives != 1):
       lives -= 1
       print(f"ცადეთ ხელთავიდან,დარჩენილი გაქვთ {lives} ცდა")
+
     else:
       print(f"სამწუხაროდ, ცდა ამოგეწურათ")
       break
+
+    if hidden_word == hidden:
+       print("ყოჩაღ! შენ სწორად გამოიცანი სიტყვა!")
+       break
        
     
 
